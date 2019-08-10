f452c2cc6d57bbb6a9ec00f39e25fa99d7804874
Brian Krausz <briankrausz@gmail.com>
2019-08-10 14:38:01 -0700

    Run fcserver as sudo

--- startup.sh
+++ startup.sh
@@ -17,8 +17,10 @@ else
   echo "No internet: not updating" >> ~/pyramid.log
 fi
 
+qjoypad &
+
 # Make sure fcserver is running
-~/fadecandy/bin/fcserver-rpi ~/pyramid/fcserver-config.json >> ~/fcserver.log 2>&1 &
+sudo ~/fadecandy/bin/fcserver-rpi ~/pyramid/fcserver-config.json >> ~/fcserver.log 2>&1 &
 
 echo "Starting Pyramid" >> ~/pyramid.log
 processing-java --sketch=/home/pi/pyramid --output=/home/pi/pyramid/out --force --run >> ~/pyramid.log 2>&1
